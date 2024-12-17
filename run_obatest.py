15711d660be5e38d58ac795f9ebe4306af90115e
EnvirEco <enviroeconomics@gmail.com>
2024-12-17 10:32:34 -0500

    Update run_obatest.py
    banking added

--- run_obatest.py
+++ run_obatest.py
@@ -51,7 +51,8 @@ def run_trading_model(start_year=2025, end_year=2035):
             'Facility ID', f'Emissions_{year}', f'Benchmark_{year}', f'Allocations_{year}',
             f'Allowance Surplus/Deficit_{year}', f'Abatement Cost_{year}',
             f'Trade Cost_{year}', f'Total Cost_{year}', f'Profit_{year}',
-            f'Costs to Profits Ratio_{year}', f'Costs to Output Ratio_{year}'
+            f'Costs to Profits Ratio_{year}', f'Costs to Output Ratio_{year}',
+            'Banked Allowances'  # Add Banked Allowances to the summary
         ]].copy()
 
         facility_summary["Year"] = year
